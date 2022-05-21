747ba9ea86403b2c0ecfdadb70d1f94abdd27271
Khoa TRAN <khoatrandinh94@gmail.com>
2022-05-21 22:46:58 +0200

    CRUD functions for REST endpoints implemented

--- app/controllers/tutorial.controller.js
+++ app/controllers/tutorial.controller.js
@@ -7,49 +7,134 @@ const Tutorial = db.tutorials;
 
 //Create and Save a new Tutorial
 exports.create = (req, res) => {
-  let msg = "Create and Save a new Tutorial";
-  console.log(msg);
-  res.json({ message: msg});
+  // Validate request
+  if (!req.body.title) {
+    res.status(400).send({ message: "Content can not be empty!" });
+    return;
+  }
+  // Create a Tutorial
+  const tutorial = new Tutorial({
+    title: req.body.title,
+    description: req.body.description,
+    published: req.body.published ? req.body.published : false
+  });
+  // Save Tutorial in the database
+  tutorial
+    .save(tutorial)
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occured during create"
+      });
+    });
 };
 
 // Retrieve all Tutorials from the database
 exports.findAll = (req, res) => {
-  let msg = "Retrieve all Tutorials from the database";
-  console.log(msg);
-  res.json({ message: msg});
+  const title = req.query.title;
+  var condition = title ? { title: { $regex: new RegExp(title), $options: "i" } } : {};
+  Tutorial.find(condition)
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while retrieving tutorials."
+      });
+    });
 };
 
 // Find all published Tutorials
 exports.findAllPublished = (req, res) => {
-  let msg = "Find all published Tutorials";
-  console.log(msg);
-  res.json({ message: msg});
+  Tutorial.find({ published: true })
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while retrieving tutorials."
+      });
+    });
 };
 
 // Find a single Tutorial with an id
 exports.findOne = (req, res) => {
-  let msg = "Find a single Tutorial with an id";
-  console.log(msg);
-  res.json({ message: msg});
+  const id = req.params.id;
+  Tutorial.findById(id)
+    .then(data => {
+      if (!data)
+        res.status(404).send({ message: "Not found Tutorial with id " + id });
+      else res.send(data);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .send({ message: "Error retrieving Tutorial with id=" + id });
+    });
 };
 
 // Update a Tutorial by the id in the request
 exports.update = (req, res) => {
-  let msg = "Update a Tutorial by the id in the request";
-  console.log(msg);
-  res.json({ message: msg});
+  if (!req.body) {
+    return res.status(400).send({
+      message: "Data to update can not be empty!"
+    });
+  }
+  const id = req.params.id;
+  Tutorial.findByIdAndUpdate(id, req.body, { useFindAndModify: false })
+    .then(data => {
+      if (!data) {
+        res.status(404).send({
+          message: `Cannot update Tutorial with id=${id}. Maybe Tutorial was not found!`
+        });
+      } else res.send({ message: "Tutorial was updated successfully." });
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Error updating Tutorial with id=" + id
+      });
+    });
 };
 
 // Delete a Tutorial by the id in the request
 exports.delete = (req, res) => {
-  let msg = "Delete a Tutorial by the id in the request";
-  console.log(msg);
-  res.json({ message: msg});
+  const id = req.params.id;
+  Tutorial.findByIdAndRemove(id)
+    .then(data => {
+      if (!data) {
+        res.status(404).send({
+          message: `Cannot delete Tutorial with id=${id}. Maybe Tutorial was not found!`
+        });
+      } else {
+        res.send({
+          message: "Tutorial was deleted successfully!"
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Could not delete Tutorial with id=" + id
+      });
+    });
 };
 
 // Delete all Tutorials from the database
 exports.deleteAll = (req, res) => {
-  let msg = "Delete all Tutorials from the database";
-  console.log(msg);
-  res.json({ message: msg});
+  Tutorial.deleteMany({})
+    .then(data => {
+      res.send({
+        message: `${data.deletedCount} Tutorials were deleted successfully!`
+      });
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while removing all tutorials."
+      });
+    });
 };
